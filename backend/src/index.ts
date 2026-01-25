4224cb1bc503738f7654a75fc3236e31b62c9706
OmRana2004 <omrana8755800322@gmail.com>
2026-01-25 14:59:20 +0530

    cleaning up the schemas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,17 @@
-import express from "express"
+import express from "express";
 import cors from "cors";
-import dotenv from "dotenv"
-import router from "./routes/pageRoutes"
+import dotenv from "dotenv";
+import router from "./routes/pageRoutes";
+
+dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/v1", router);
 
-app.use("/api/v1/", router)
-
-app.listen(3000, () => {
-    console.log("server is running on port 3000")
-})
+app.listen(3001, () => {
+  console.log("server is running on port 3001");
+});
--- backend/src/routes/pageRoutes.ts
+++ backend/src/routes/pageRoutes.ts
@@ -3,9 +3,9 @@ import signup from "../controllers/authController/signup";
 import signin from "../controllers/authController/signin";
 import authMiddleware from "../middlewares/authMiddleware";
 import wallet from "../controllers/crudController/wallet";
-import balance from "../controllers/crudController/balance";
-import findUser from "../controllers/crudController/findUser";
 import transaction from "../controllers/crudController/transaction";
+import findUser from "../controllers/crudController/findUser";
+import balance from "../controllers/crudController/balance";
 
 
 
@@ -15,8 +15,8 @@ const AuthMiddleware = authMiddleware as unknown as RequestHandler;
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/wallet", AuthMiddleware, wallet as unknown as RequestHandler);
-router.post("/user", findUser);
 router.post("/transaction", AuthMiddleware, transaction as unknown as RequestHandler);
-router.get("/balance", AuthMiddleware, balance as unknown as RequestHandler)
+router.post("/user", findUser);
+router.get("/balance", AuthMiddleware, balance as unknown as RequestHandler);
 
 export default router;
